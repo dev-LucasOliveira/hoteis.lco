60b5fb615a2c9d9f0d4c00a64914159415209812
the-pi-user <105022212+the-pi-user@users.noreply.github.com>
2022-08-01 15:53:35 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,147 @@
+let imageArray1 = [
+	
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/245809510.jpg?k=a0174fec14edc889db46231c149567015bd9d1a51d3a8498868a4df2dbf7c67c&o=&hp=1", 
+    
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/245815248.jpg?k=372c6478f1824fbbde5e084861f10380f1c158155bf2514c1095192047a1876f&o=&hp=1", 
+      
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/245809699.jpg?k=f58124df4a078793c107363b4ae7f99de25112aacbd6dd778ba508ddb5c3b6a4&o=&hp=1", 
+    
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/234701770.jpg?k=b54d84a2afdd1ce3512ef203bb9d186b888546c05be57b24cd33e4c07412512c&o=&hp=1",
+
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/276138350.jpg?k=7791874aaa663e0c4cf163d3e5a6bb672d8f5e597702caa9bb157388485b02b4&o=&hp=1",
+
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/245816892.jpg?k=2103a9aa496fd234e220a3f030c6ba03226b87aa114e6a7c4ad272870a896a58&o=&hp=1",
+
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/271185741.jpg?k=072ff28072187c662929a8296d8a98b03335dc8fd8c8e6fc6e0f518835a56aab&o=&hp=1",
+
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/245760751.jpg?k=3fcb8402356d1a5fef5f1e41267d0aa8aec00939f6cc68d029544ef0abadd002&o=&hp=1",
+
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/245760306.jpg?k=8ab357e0084ab50e5664711b2e4d028fe92bac24a64d7eab41ab7d25816a634e&o=&hp=1"
+
+];
+
+let imageArray2 = [
+	
+    "https://cache.marriott.com/content/dam/marriott-renditions/BOBMD/bobmd-pontoons-1605-hor-clsc.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1215px:*", 
+        
+    "https://cache.marriott.com/content/dam/marriott-renditions/BOBMD/bobmd-king-overwater-bungalow-1905-hor-clsc.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1215px:*", 
+          
+    "https://cache.marriott.com/content/dam/marriott-renditions/BOBMD/bobmd-lagoon-bungalows-2787-hor-clsc.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1215px:*",
+    
+    "https://cache.marriott.com/content/dam/marriott-renditions/BOBMD/bobmd-exterior-2912-hor-clsc.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1215px:*",
+    
+    "https://cache.marriott.com/content/dam/marriott-renditions/BOBMD/bobmd-view-2384-hor-clsc.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1215px:*",
+
+    "https://cache.marriott.com/content/dam/marriott-renditions/BOBMD/bobmd-villa-2964-hor-clsc.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1215px:*",
+
+    "https://cache.marriott.com/content/dam/marriott-renditions/BOBMD/bobmd-villa-2970-hor-clsc.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1215px:*",
+    
+    "https://cache.marriott.com/content/dam/marriott-renditions/BOBMD/bobmd-villa-6367-hor-clsc.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1215px:*",
+
+    "https://cache.marriott.com/content/dam/marriott-renditions/BOBMD/bobmd-villa-6467-hor-clsc.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1215px:*"
+    
+];
+
+let imageArray3 = [
+	
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/327275271.jpg?k=74702ba062bb9b97f4c847de6c0d71737d6eff196454bc2da6e097438790495f&o=&hp=1", 
+    
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/275926399.jpg?k=41cd628c033c7d4827ae04c783101ee94df3751d8c496fd66e7af478ed0ebd09&o=&hp=1", 
+      
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/279367142.jpg?k=a417fb98f13ef97fcd40b8fdab7ea7dd86b68dacbbd5c526e197f9ad2baf3d4c&o=&hp=1", 
+    
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/245988897.jpg?k=90964796ddfc2b6c5fa2166c878c07d26bff9c50adb85c665ca24d0b672f78a7&o=&hp=1",
+
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/245989104.jpg?k=8a331587f8df9dd56352eb9a4e24826792532f91f91f7e071bf01ed442872315&o=&hp=1",
+
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/245988398.jpg?k=4b9d3e6f9d0b45b8562bffab579624fb00b2fea2f74a327c73184032ea9d3ef5&o=&hp=1",
+
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/245995047.jpg?k=f9122f92b8c643217896ea6a7006962a91d99022a2a0fd33260036275fdc52e6&o=&hp=1",
+
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/126678159.jpg?k=4707d6ee4402c8d7436a6800ee77e60cf8b48c6f516d22546ccd2f50c2aad6ea&o=&hp=1",
+
+    "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/279366644.jpg?k=57424e21238fc69f80f426b84f088a3617bd53e212c7faf2f8c59b7955840929&o=&hp=1"
+
+];
+
+
+let imageHTML = document.getElementById('imageHTML');
+
+let imageIndex = 0;
+
+
+function change(btnNumber) {
+
+    if (btnNumber === 1) {
+        imageIndex = imageIndex - 1;
+	    imageHTML.src = imageArray1[imageIndex]; 
+        
+        if (imageIndex < 0) {
+            imageIndex = imageIndex + 9;
+            imageHTML.src = imageArray1[imageIndex];
+        }
+    }
+    
+    if (btnNumber === 2) {
+        imageHTML.src = "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/245809510.jpg?k=a0174fec14edc889db46231c149567015bd9d1a51d3a8498868a4df2dbf7c67c&o=&hp=1";
+    }
+
+    if (btnNumber === 3) {
+        imageIndex = imageIndex + 1;
+        imageHTML.src = imageArray1[imageIndex];
+
+        if (imageIndex > 8) {
+            imageIndex = imageIndex - 9;
+            imageHTML.src = imageArray1[imageIndex];
+        }    
+    }
+    
+    if (btnNumber === 4) {
+        imageIndex = imageIndex - 1;
+	    imageHTML.src = imageArray2[imageIndex]; 
+        
+        if (imageIndex < 0) {
+            imageIndex = imageIndex + 9;
+            imageHTML.src = imageArray2[imageIndex];
+        }
+    }
+    
+    if (btnNumber === 5) {
+        imageHTML.src = "https://cache.marriott.com/content/dam/marriott-renditions/BOBMD/bobmd-pontoons-1605-hor-clsc.jpg?output-quality=70&interpolation=progressive-bilinear&downsize=1215px:*";
+    }
+
+    if (btnNumber === 6) {
+        imageIndex = imageIndex + 1;
+        imageHTML.src = imageArray2[imageIndex];
+
+        if (imageIndex > 8) {
+            imageIndex = imageIndex - 9;
+            imageHTML.src = imageArray2[imageIndex];
+        }
+    }
+    
+    if (btnNumber === 7) {
+        imageIndex = imageIndex - 1;
+	    imageHTML.src = imageArray3[imageIndex]; 
+        
+        if (imageIndex < 0) {
+            imageIndex = imageIndex + 9;
+            imageHTML.src = imageArray3[imageIndex];
+        }  
+    }
+    
+    if (btnNumber === 8) {
+        imageHTML.src = "https://t-cf.bstatic.com/xdata/images/hotel/max1024x768/327275271.jpg?k=74702ba062bb9b97f4c847de6c0d71737d6eff196454bc2da6e097438790495f&o=&hp=1";
+    }
+
+    if (btnNumber === 9) {
+        imageIndex = imageIndex + 1;
+        imageHTML.src = imageArray3[imageIndex];
+
+        if (imageIndex > 8) {
+            imageIndex = imageIndex - 9;
+            imageHTML.src = imageArray3[imageIndex];
+        }
+    }
+
+}
